4af2d2775a1082f560e4772dafa713bf21ae7235
Seongbo Jang <jang.sb@postech.ac.kr>
2024-08-09 19:23:03 +0900

    Shuffle the answer index for debiasing few-shot (cloze-test_gen)

--- lm_eval/tasks/timelychat/cloze_gen/utils.py
+++ lm_eval/tasks/timelychat/cloze_gen/utils.py
@@ -1,18 +1,28 @@
+import random
 from typing import Dict, List, Union
 
+import datasets
 
-def doc_to_text_instruct(doc: Dict[str, Union[str, List[str]]]) -> str:
-    context = "\n".join([f"{speaker}: {utt}" for speaker, utt in zip(doc["speaker_list"], doc["context"])])
 
-    query = (
-        "### Dialogue context ###\n"
-        f"{context}\n\n"
-        "### Time elapsed ###\n"
-        f"{doc['time_elapsed']}\n\n"
-        f"### Next response ###\n"
-        f"1. {doc['target_speaker']}: {doc['timely_response']}\n"
-        f"2. {doc['target_speaker']}: {doc['untimely_response']}\n\n"
-        "Answer:"
-    )
+def process_docs_instruct(dataset: datasets.Dataset):
+    def _helper(doc):
+        doc["choices"] = [doc["timely_response"], doc["untimely_response"]]
+        random.shuffle(doc["choices"])
+        doc["gold"] = doc["choices"].index(doc["timely_response"]) + 1
 
-    return query
+        context = "\n".join([f"{speaker}: {utt}" for speaker, utt in zip(doc["speaker_list"], doc["context"])])
+        query = (
+            "### Dialogue context ###\n"
+            f"{context}\n\n"
+            "### Time elapsed ###\n"
+            f"{doc['time_elapsed']}\n\n"
+            f"### Next response ###\n"
+            f"1. {doc['target_speaker']}: {doc['choices'][0]}\n"
+            f"2. {doc['target_speaker']}: {doc['choices'][1]}\n\n"
+            "Answer:"
+        )
+        doc["query"] = query
+
+        return doc
+
+    return dataset.map(_helper)
