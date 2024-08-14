92085b8de749a145ec67ff3160b7bcd203f86b9a
Seongbo Jang <jang.sb@postech.ac.kr>
2024-08-14 15:47:36 +0900

    Add tasks for MSC model

--- lm_eval/tasks/timelychat/open-ended_gen/utils.py
+++ lm_eval/tasks/timelychat/open-ended_gen/utils.py
@@ -3,7 +3,6 @@ from typing import Dict, List, Union
 
 def doc_to_text_instruct(doc: Dict[str, Union[str, List[str]]]) -> str:
     context = "\n".join([f"{speaker}: {utt}" for speaker, utt in zip(doc["speaker_list"], doc["context"])])
-
     # Refer to Table 12 of https://arxiv.org/abs/2402.13211
     query = (
         "### Dialogue context ###\n"
@@ -24,5 +23,23 @@ def doc_to_text_rebot(doc: Dict[str, Union[str, List[str]]]) -> str:
     """
     query = " ".join([f"<{speaker}> {utt}" for speaker, utt in zip(doc["speaker_list"], doc["context"])])
     query += f" <{doc['time_elapsed'].capitalize()} after> <{doc['target_speaker']}>"
+
+    return query
+
+
+def doc_to_text_msc(doc: Dict[str, Union[str, List[str]]]) -> str:
+    """
+    The dialogue format used in MSC (https://aclanthology.org/2022.acl-long.356/).
+    ex) p1: u1\np2: u2\n ... \ntime: t
+    """
+    speaker_mapping = {"A": "p1", "B": "p2"}
+    query = ""
+    for speaker, utt in zip(doc["speaker_list"], doc["context"]):
+        if speaker in speaker_mapping.keys():
+            speaker = speaker_mapping[speaker]
+        query += f"{speaker}: {utt}\n"
+
+    target_speaker = speaker_mapping[doc["target_speaker"]] if doc["target_speaker"] in speaker_mapping.keys() else doc["target_speaker"]
+    query += f"time: {doc['time_elapsed']}\n{target_speaker}:"
 
     return query
