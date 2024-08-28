6a6414f763f55d78fc434f2e46d5756059e2e0d6
Seongbo Jang <jang.sb@postech.ac.kr>
2024-08-28 21:48:55 +0900

    Add CoT to Time interval prediction

--- lm_eval/tasks/timelychat/time_interval/cloze_gen/utils.py
+++ lm_eval/tasks/timelychat/time_interval/cloze_gen/utils.py
@@ -29,6 +29,32 @@ def process_docs_instruct_without_gold_response(dataset: datasets.Dataset):
     return dataset.map(_helper)
 
 
+def process_docs_instruct_without_gold_response_cot(dataset: datasets.Dataset):
+    def _helper(doc):
+        doc["choices"] = [doc["time_elapsed"].lower(), *list(map(lambda x: x.lower(), doc["negative_answers"]))]
+        random.shuffle(doc["choices"])
+        answer_idx = doc["choices"].index(doc["time_elapsed"].lower())
+        answer_map = {0: "A", 1: "B", 2: "C", 3: "D"}
+        doc["gold"] = answer_map[answer_idx]
+
+        context = "\n".join([f"{speaker}: {utt}" for speaker, utt in zip(doc["speaker_list"], doc["context"])])
+        query = (
+            "### Dialogue context ###\n"
+            f"{context}\n\n"
+            f"Question: How much time is needed for {doc['target_speaker']} to respond?\n"
+            f"A. {doc['choices'][0]}\n"
+            f"B. {doc['choices'][1]}\n"
+            f"C. {doc['choices'][2]}\n"
+            f"D. {doc['choices'][3]}\n"
+            "Answer: Let's think step by step."
+        )
+        doc["query"] = query
+
+        return doc
+
+    return dataset.map(_helper)
+
+
 def process_docs_instruct_with_gold_response(dataset: datasets.Dataset):
     def _helper(doc):
         doc["choices"] = [doc["time_elapsed"].lower(), *list(map(lambda x: x.lower(), doc["negative_answers"]))]
@@ -49,6 +75,34 @@ def process_docs_instruct_with_gold_response(dataset: datasets.Dataset):
             f"C. {doc['choices'][2]}\n"
             f"D. {doc['choices'][3]}\n"
             "Answer:"
+        )
+        doc["query"] = query
+
+        return doc
+
+    return dataset.map(_helper)
+
+
+def process_docs_instruct_with_gold_response_cot(dataset: datasets.Dataset):
+    def _helper(doc):
+        doc["choices"] = [doc["time_elapsed"].lower(), *list(map(lambda x: x.lower(), doc["negative_answers"]))]
+        random.shuffle(doc["choices"])
+        answer_idx = doc["choices"].index(doc["time_elapsed"].lower())
+        answer_map = {0: "A", 1: "B", 2: "C", 3: "D"}
+        doc["gold"] = answer_map[answer_idx]
+
+        context = "\n".join([f"{speaker}: {utt}" for speaker, utt in zip(doc["speaker_list"], doc["context"])])
+        query = (
+            "### Dialogue context ###\n"
+            f"{context}\n\n"
+            "### Target response ###\n"
+            f"{doc['target_speaker']}: {doc['timely_response']}\n\n"
+            f"Question: How much time is needed for {doc['target_speaker']} to respond with the target response?\n"
+            f"A. {doc['choices'][0]}\n"
+            f"B. {doc['choices'][1]}\n"
+            f"C. {doc['choices'][2]}\n"
+            f"D. {doc['choices'][3]}\n"
+            "Answer: Let's think step by step."
         )
         doc["query"] = query
 
